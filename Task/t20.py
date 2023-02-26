f85f8de6001b075c7b0a3066e623538377934bdc
ZiganshinIB <ZiganshinIB@nknh.sibur.ru>
2023-02-26 17:38:32 +0300

    Refract t20

--- Task/t20.py
+++ Task/t20.py
@@ -25,3 +25,14 @@ def scrabble_2(text: str, score_data: dict) -> int:
     for c in list(text.upper()):
         score += score_data[c]
     return score
+
+
+if __name__ == "__main__":
+    score_data = {}
+    dict_scrabble_list_machin(score_data,
+                              texts=["AEIOULNSTR", 'DG', 'BCMP', 'FHVWY', 'K', 'JX', 'QZ',
+                                         'АВЕИНОРСТ', 'ДКЛМПУ', 'БГЁЬЯ', 'ЙЫ', 'ЖЗХЦЧ', 'ШЭЮ', 'ФЩЪ'],
+                              scores=[1, 2, 3, 4, 5, 8, 10,
+                                      1, 2, 3, 4, 5, 8, 10])
+
+    print(scrabble_2('ноутбук', score_data))
